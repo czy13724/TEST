b803676a1b4607d6e3421995c66b6e078e1c22b6
czy13724 <103307567+czy13724@users.noreply.github.com>
2023-11-27 16:45:11 +0800

    Create generate_json.py

---
+++ .github/scripts/generate_json.py
@@ -0,0 +1,23 @@
+import os
+import json
+
+def generate_json():
+    image_folder = 'test'
+    json_data = {
+        "name": "TEST图标订阅",
+        "description": "收集一些自己常用的图标，更新日期：xxx",
+        "icons": []
+    }
+
+    for filename in os.listdir(image_folder):
+        if filename.endswith(".png"):
+            image_path = os.path.join(image_folder, filename)
+            raw_url = f"https://raw.githubusercontent.com/{os.environ['GITHUB_REPOSITORY']}/main/{image_path}"
+            icon_data = {"name": filename, "url": raw_url}
+            json_data["icons"].append(icon_data)
+
+    with open('icons.json', 'w') as json_file:
+        json.dump(json_data, json_file)
+
+if __name__ == "__main__":
+    generate_json()
