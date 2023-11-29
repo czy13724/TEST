4ac5ee6e05638e42f30275186ef01836a44f9dd0
czy13724 <103307567+czy13724@users.noreply.github.com>
2023-11-29 11:23:17 +0800

    Update Generate_task_jsonfile.py

--- .github/scripts/Generate_task_jsonfile.py
+++ .github/scripts/Generate_task_jsonfile.py
@@ -21,12 +21,15 @@ def fetch_image_url(script_name, image_folder):
         print(f"Image file {image_file} not found in the image folder.")
         return None
 
+
 def execute_script(script_url):
     script_content = fetch_script(script_url)
     if script_content:
         # 替换URL为注释，避免语法错误
         script_content = script_content.replace("https://", "# https://")
-        exec(script_content, globals())
+        print(f"Executing script:\n{script_content}")
+        exec(script_content, globals(), {})
+
 
 # 仓库根目录路径，包含上传的 JavaScript 和配置文件
 repo_root = os.path.join(os.getcwd(), "..")
