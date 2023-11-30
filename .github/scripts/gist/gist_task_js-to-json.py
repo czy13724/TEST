b1fab159df266e5c7a9321af30036e4ef3fc747f
czy13724 <103307567+czy13724@users.noreply.github.com>
2023-11-30 18:31:36 +0800

    Update gist_task_js-to-json.py

--- .github/scripts/gist/gist_task_js-to-json.py
+++ .github/scripts/gist/gist_task_js-to-json.py
@@ -66,7 +66,7 @@ def generate_task_json():
                     image_filename = similar_images[0]
 
                 # 添加其余信息到 task_entry
-                task_entry["config"] += f"https://gist.githubusercontent.com/{github_username}/{gist_id}/raw/main/{js_file['filename']}, tag={file_name_without_extension}, img-url=https://raw.githubusercontent.com/{github_username}/{gist_id}/main/image/{image_filename}, enabled=false"
+                task_entry["config"] += f"https://gist.githubusercontent.com/{github_username}/{gist_id}/raw/main/{js_file['filename']}, tag={file_name_without_extension}, img-url=https://raw.githubusercontent.com/{github_username}/TEST/main/image/{image_filename}, enabled=false"
 
                 # 将 task_entry 添加到 result 字典中
                 result["task"].append(task_entry)
