e7113c39ce5f8efeb60ed109de73d5946bf7ccc2
czy13724 <103307567+czy13724@users.noreply.github.com>
2023-11-30 11:17:33 +0800

    Update get_gist_id.py

--- .github/scripts/gist/get_gist_id.py
+++ .github/scripts/gist/get_gist_id.py
@@ -1,42 +1,32 @@
-import os
 import requests
+import os
 
-script_folder = os.path.dirname(os.path.abspath(__file__))
-print(f"Script folder: {script_folder}")
+# 设置GitHub用户名和Gist访问令牌
+username = "czy13724"
+token = "GETGISTID"
 
-# 之后再执行文件的其他部分
+# 创建一个空的gist_ids.txt文件
+with open("gist_ids.txt", "w") as file:
+    pass
 
+# 发送GET请求获取用户的私有Gists列表
+url = f"https://api.github.com/users/{username}/gists"
+headers = {"Authorization": f"Token {token}"}
+response = requests.get(url, headers=headers)
 
-# GitHub 用户名
-github_username = "czy13724"
-
-# Gist Token
-gist_token = "GETGISTID"
-
-def get_private_gist_id(username, token):
-    headers = {
-        "Authorization": f"token {token}"
-    }
-
-    # 获取用户的 Gists 列表
-    gists_url = f"https://api.github.com/users/{username}/gists"
-    response = requests.get(gists_url, headers=headers)
-
-    if response.status_code != 200:
-        print(f"Failed to fetch Gists: {response.text}")
-        return
-
-    print(f"GitHub Token: {github_token}")
-    print(f"Fetching Gists from: {gists_url}")
-    
+# 解析响应并提取每个私有Gist的raw链接
+if response.status_code == 200:
     gists = response.json()
-
-    # 遍历 Gists，并将每个 Gist 的 ID 写入文件
-    with open("gist_ids.txt", "w") as id_file:
-        for gist in gists:
-            gist_id = gist["id"]
-            id_file.write(f"{gist_id}\n")
-
-if __name__ == "__main__":
-    # 获取私有 Gist 的 ID
-    get_private_gist_id(github_username, gist_token)
+    for gist in gists:
+        gist_id = gist["id"]
+        files = gist["files"]
+        for file in files.values():
+            filename = file["filename"]
+            if filename.endswith((".js", ".conf")):
+                raw_url = file["raw_url"]
+                # 将Gist ID写入gist_ids.txt文件
+                with open("gist_ids.txt", "a") as file:
+                    file.write(gist_id + "\n")
+                print(f"{filename}的Raw链接：{raw_url}")
+else:
+    print("无法获取Gists。")
