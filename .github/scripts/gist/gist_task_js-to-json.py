7c34cd620d3b7ec74328254216bb6a9f28288053
czy13724 <103307567+czy13724@users.noreply.github.com>
2023-11-30 14:50:01 +0800

    Update gist_task_js-to-json.py

--- .github/scripts/gist/gist_task_js-to-json.py
+++ .github/scripts/gist/gist_task_js-to-json.py
@@ -1,6 +1,5 @@
 import os
 import requests
-import json
 
 def get_gist_raw_links(username, token, repository, file_extension):
     # GitHub API 地址
@@ -11,23 +10,22 @@ def get_gist_raw_links(username, token, repository, file_extension):
 
     # 发送 GET 请求获取 Gist 列表
     response = requests.get(api_url, headers=headers)
-    gists = response.json()
 
-    # 打印 gists 查看具体内容
-    print("Gists Response:")
-    print(json.dumps(gists, indent=2))
+    # 检查请求是否成功
+    if response.status_code != 200:
+        print(f"Error getting Gists. Status Code: {response.status_code}")
+        print(response.json())
+        return []
+
+    gists = response.json()
 
     # 存储符合条件的文件的 raw 链接
     raw_links = []
 
     # 遍历每个 Gist
     for gist in gists:
-        # 打印 gist 查看具体内容
-        print("Gist:")
-        print(json.dumps(gist, indent=2))
-        
-        files = gist["files"]
-        
+        files = gist.get("files", {})
+
         # 遍历每个文件
         for filename, file_data in files.items():
             # 获取文件后缀
@@ -36,8 +34,9 @@ def get_gist_raw_links(username, token, repository, file_extension):
             # 只处理指定后缀的文件
             if file_extension_in_gist == file_extension:
                 # 获取文件的 raw 链接
-                raw_url = file_data["raw_url"]
-                raw_links.append(raw_url)
+                raw_url = file_data.get("raw_url", "")
+                if raw_url:
+                    raw_links.append(raw_url)
 
     return raw_links
 
@@ -51,13 +50,4 @@ if __name__ == "__main__":
 
     # 获取 conf 和 js 文件的 raw 链接
     conf_raw_links = get_gist_raw_links(github_username, github_token, repository_name, "conf")
-    js_raw_links = get_gist_raw_links(github_username, github_token, repository_name, "js")
-
-    # 打印结果
-    print("Conf Raw Links:")
-    for link in conf_raw_links:
-        print(link)
-
-    print("\nJS Raw Links:")
-    for link in js_raw_links:
-        print(link)
+    js_raw_links = get_gist_raw_links(github_username, g
