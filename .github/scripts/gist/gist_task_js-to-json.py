20855c7cc9c9aa79443fd43fc0d3a3bc42a15e5a
czy13724 <103307567+czy13724@users.noreply.github.com>
2023-11-30 15:06:23 +0800

    Update gist_task_js-to-json.py

--- .github/scripts/gist/gist_task_js-to-json.py
+++ .github/scripts/gist/gist_task_js-to-json.py
@@ -4,75 +4,20 @@ import json
 import random
 from difflib import get_close_matches
 
-def get_gist_files(gist_id, github_username, repository_name, image_folder):
-    # Gist 文件列表的 API 地址
-    api_url = f"https://api.github.com/gists/{gist_id}"
-
-    # 发送请求，获取 Gist 文件列表
-    response = requests.get(api_url)
-    
-    # 检查请求是否成功
-    if response.status_code == 200:
-        # 解析响应的 JSON
-        gist_data = response.json()
-
-        # 获取 Gist 中的文件信息
-        files = gist_data["files"]
-
-        # 创建一个 task_entry 字典，用于表示每个脚本的信息
-        task_entry = {"config": "", "addons": ""}
-
-        # 遍历每个文件
-        for filename, file_data in files.items():
-            # 获取文件后缀
-            file_extension = filename.split(".")[-1]
-
-            # 只处理后缀为 js 和 conf 的文件
-            if file_extension in ["js", "conf"]:
-                # 获取文件的 raw 链接
-                raw_url = file_data["raw_url"]
-
-                # 获取文件名，不带后缀
-                file_name_without_extension = filename.rsplit(".", 1)[0]
-
-                # 随机生成 cron 表达式
-                cron_expression = f"{random.randint(0, 59)} {random.randint(0, 23)} * * *"
-
-                # 添加 cron 表达式到 task_entry
-                task_entry["config"] += f"{cron_expression} {raw_url}, tag={file_name_without_extension}, img-url="
-
-                # 寻找相似的图片文件名
-                similar_images = get_close_matches(file_name_without_extension, os.listdir(image_folder), n=1)
-
-                # 如果找到相似的图片文件名，添加图片的 raw 链接
-                if similar_images:
-                    image_filename = similar_images[0]
-                    task_entry["config"] += f"https://raw.githubusercontent.com/{github_username}/{repository_name}/main/image/{image_filename}"
-
-                # 添加其余信息到 task_entry
-                task_entry["config"] += f", enabled=false"
-
-        return task_entry
-
-    else:
-        print(f"Failed to retrieve Gist files. Status code: {response.status_code}")
-        return None
-
 def generate_task_json():
-    # GitHub 用户名和仓库名
+    # GitHub 用户名
     github_username = "czy13724"
-    repository_name = "TEST"
 
-    # GitHub Token
+    # 获取 GitHub Token
     github_token = os.getenv("GETGISTID")
     headers = {"Authorization": f"token {github_token}"}
 
-    # 获取 Gist ID 列表的 API 地址
+    # 获取 Gist 列表的 API 地址
     api_url = f"https://api.github.com/users/{github_username}/gists"
 
-    # 发送 GET 请求获取 Gist ID 列表
+    # 发送请求，获取 Gist 列表
     response = requests.get(api_url, headers=headers)
-    
+
     # 检查请求是否成功
     if response.status_code == 200:
         # 解析响应的 JSON
@@ -85,19 +30,46 @@ def generate_task_json():
             "task": []
         }
 
-        # 文件夹路径，包含上传的图片文件
-        image_folder = os.path.join(os.getcwd(), "image")
-
-        # 遍历每个 Gist
+        # 遍历 Gist 列表
         for gist in gists:
             gist_id = gist["id"]
-            
-            # 获取 Gist 中的文件信息
-            task_entry = get_gist_files(gist_id, github_username, repository_name, image_folder)
-            
-            # 如果获取文件信息成功，将 task_entry 添加到 result 字典中
-            if task_entry:
-                result["task"].append(task_entry)
+            files = gist["files"]
+
+            # 遍历每个文件
+            for filename, file_data in files.items():
+                # 获取文件后缀
+                file_extension = filename.split(".")[-1]
+
+                # 只处理后缀为 js 和 conf 的文件
+                if file_extension in ["js", "conf"]:
+                    # 获取文件的 raw 链接
+                    raw_url = file_data["raw_url"]
+
+                    # 获取文件名，不带后缀
+                    file_name_without_extension = filename.rsplit(".", 1)[0]
+
+                    # 创建一个 task_entry 字典，用于表示每个脚本的信息
+                    task_entry = {"config": "", "addons": ""}
+
+                    # 随机生成 cron 表达式
+                    cron_expression = f"{random.randint(0, 59)} {random.randint(0, 23)} * * *"
+
+                    # 添加 cron 表达式到 task_entry
+                    task_entry["config"] += f"{cron_expression} {raw_url}, tag={file_name_without_extension}, img-url="
+
+                    # 寻找相似的图片文件名
+                    similar_images = get_close_matches(file_name_without_extension, os.listdir("image"), n=1)
+
+                    # 如果找到相似的图片文件名，添加图片的 raw 链接
+                    if similar_images:
+                        image_filename = similar_images[0]
+                        task_entry["config"] += f"https://raw.githubusercontent.com/{github_username}/{gist_id}/main/image/{image_filename}"
+
+                    # 添加其余信息到 task_entry
+                    task_entry["config"] += f", enabled=false"
+
+                    # 将 task_entry 添加到 result 字典中
+                    result["task"].append(task_entry)
 
         # 将结果输出到 JSON 文件
         output_file_path = os.path.join(os.getcwd(), "test.gallery.json")
@@ -107,7 +79,6 @@ def generate_task_json():
         # 打印文件是否存在以及路径
         print(f"File exists: {os.path.exists(output_file_path)}")
         print(f"File path: {output_file_path}")
-
     else:
         print(f"Failed to retrieve Gist list. Status code: {response.status_code}")
 
