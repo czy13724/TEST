5854259d29a6a8c53ddede4b132e7bc8d0a1a047
czy13724 <103307567+czy13724@users.noreply.github.com>
2023-11-30 14:08:01 +0800

    Update gist_task_js-to-json.py

--- .github/scripts/gist/gist_task_js-to-json.py
+++ .github/scripts/gist/gist_task_js-to-json.py
@@ -3,7 +3,14 @@ import re
 import requests
 import json
 import random
-from random import randint
+from dotenv import load_dotenv
+
+# 加载环境变量
+load_dotenv()
+
+# 获取 GITHUB_TOKEN 和 GIST_ID 的值
+GITHUB_TOKEN = os.getenv("GITHUB_TOKEN")
+GIST_ID = os.getenv("GIST_ID")
 
 # 仓库根目录路径，包含上传的 JavaScript 和配置文件
 repo_root = os.path.join(os.getcwd(), "..")
@@ -13,20 +20,32 @@ scripts_folder = os.path.join(repo_root, "TEST", "javascript")
 image_folder = os.path.join(repo_root, "TEST", "image")
 conf_folder = os.path.join(repo_root, "TEST", "conf")
 
-# 修改这里的 GIST_URL，用于获取 Gist 列表，替换为你的 Gist URL
-GIST_URL = "https://api.github.com/users/czy13724/gists"
-GIST_HEADERS = {
-    "Authorization": f"Bearer {os.environ['GETGISTID']}"
-}
+# 获取 Gist ID 的函数
+def get_gist_id(token, gist_name):
+    headers = {"Authorization": f"token {token}"}
+    response = requests.get(f"https://api.github.com/gists/{GIST_ID}", headers=headers)
+    if response.status_code == 200:
+        gist_info = response.json()
+        for file_name, file_info in gist_info["files"].items():
+            if file_name == gist_name:
+                return file_info["raw_url"]
+    return None
 
-def fetch_script(script_url):
-    response = requests.get(script_url)
-    return response.text if response.status_code == 200 else None
+# 获取图片文件的 raw URL 的函数
+def fetch_image_raw_url(image_file_name):
+    # 构建图片文件的 raw URL
+    raw_url = f"https://raw.githubusercontent.com/czy13724/TEST/main/image/{image_file_name}"
+    return raw_url
 
+# 获取 Gist 文件的 raw URL 的函数
+def fetch_gist_raw_url(gist_id, file_name):
+    return f"https://gist.githubusercontent.com/czy13724/{gist_id}/raw/{file_name}"
+
+# 执行 JavaScript 脚本的函数
 def execute_script(script_url):
     script_content = fetch_script(script_url)
     if script_content:
-        # 替换URL为注释，避免语法错误
+        # 替换 URL 为注释，避免语法错误
         script_content = script_content.replace("https://", "# https://")
 
         # 在脚本开头添加导入语句
@@ -40,46 +59,51 @@ import requests
         print(f"Executing script:\n{script_content}")
         exec(script_content, globals(), {})
 
-def get_gist_id(gist_url):
-    # 匹配 Gist URL 中的 Gist ID
-    match = re.search(r'/([a-f0-9]+)/', gist_url)
-    return match.group(1) if match else None
+# 获取 JavaScript 脚本的函数
+def fetch_script(script_url):
+    response = requests.get(script_url)
+    return response.text if response.status_code == 200 else None
 
-def fetch_gist_raw_url(gist_id, file_name):
-    # 构建 Gist 文件的 raw URL
-    raw_url = f"https://gist.githubusercontent.com/{os.environ['GITHUB_ACTOR']}/{gist_id}/raw/{file_name}"
-    return raw_url
-
+# 创建一个 result 字典用于存放任务信息
 def generate_task_json(gists):
     result = {
-        "name": "Levi任务合集订阅",
-        "description": "如有侵权请联系@PMLevibot删除。tg机器人：https://t.me/PMLevibot",
+        "name": "Levi Task Collection Subscription",
+        "description": "Contact @PMLevibot to delete if there is any infringement. Telegram bot: https://t.me/PMLevibot",
         "task": []
     }
 
+    # 遍历获取到的 Gist 信息
     for gist in gists:
-        # 遍历 Gist 中的每个文件
-        for gist_file in gist["files"].values():
-            file_name = gist_file["filename"]
-            if file_name.endswith((".js", ".conf")):
-                # 创建一个 task_entry 字典，用于表示每个脚本的信息
-                task_entry = {"config": "", "addons": ""}
+        # 获取 Gist 的文件信息
+        files = gist["files"]
 
-                # 随机生成 cron 表达式
-                cron_expression = f"{random.randint(0, 59)} {random.randint(0, 23)} * * *"
+        # 检查是否有 JavaScript 文件
+        js_files = [file_name for file_name in files if file_name.endswith(".js")]
 
-                # 添加 cron 表达式到 task_entry
-                task_entry["config"] += f"{cron_expression} {fetch_gist_raw_url(gist['id'], file_name)}, tag={file_name[:-3]}, img-url={fetch_image_raw_url(file_name)}, enabled=false"
+        # 如果没有 JavaScript 文件，则跳过
+        if not js_files:
+            continue
 
-                # 判断是否有配置文件，决定是否添加 addons 字段
-                conf_files = [conf_file for conf_file in os.listdir(conf_folder) if conf_file.startswith(file_name[:-3]) and conf_file.endswith(".conf")]
-                if conf_files:
-                    # 如果有配置文件，则添加 addons 字段
-                    conf_file = conf_files[0]  # 只取第一个配置文件，你的需求是一个脚本对应一个配置文件
-                    task_entry["addons"] = f"{fetch_gist_raw_url(gist['id'], conf_file)}, tag={file_name[:-3]}"
+        # 取第一个 JavaScript 文件，你的需求是一个 Gist 对应一个 JavaScript 文件
+        js_file = js_files[0]
 
-                # 将 task_entry 添加到 result 字典中
-                result["task"].append(task_entry)
+        # 获取 cron 表达式
+        cron_expression = f"{random.randint(0, 59)} {random.randint(0, 23)} * * *"
+
+        # 创建一个 task_entry 字典，用于表示每个脚本的信息
+        task_entry = {"config": "", "addons": ""}
+
+        # 添加 cron 表达式到 task_entry
+        task_entry["config"] += f"{cron_expression} {fetch_gist_raw_url(GIST_ID, js_file)}, tag={js_file[:-3]}, img-url={fetch_image_raw_url(js_file)}, enabled=false"
+
+        # 判断是否有配置文件，决定是否添加 addons 字段
+        conf_file = js_file[:-2] + "conf"
+        if conf_file in files:
+            # 如果有配置文件，则添加 addons 字段
+            task_entry["addons"] = f"{fetch_gist_raw_url(GIST_ID, conf_file)}, tag={js_file[:-3]}"
+
+        # 将 task_entry 添加到 result 字典中
+        result["task"].append(task_entry)
 
     # 将结果输出到 JSON 文件
     output_file_path = os.path.join(os.getcwd(), "test.gallery.json")
@@ -91,9 +115,12 @@ def generate_task_json(gists):
     print(f"File path: {output_file_path}")
 
 if __name__ == "__main__":
-    # 获取用户的 Gists
-    response = requests.get(GIST_URL, headers=GIST_HEADERS)
-    gists = response.json()
-
-    # 生成任务 JSON
-    generate_task_json(gists)
+    # 获取 Gist 信息
+    headers = {"Authorization": f"token {GITHUB_TOKEN}"}
+    response = requests.get(f"https://api.github.com/gists/{GIST_ID}", headers=headers)
+    if response.status_code == 200:
+        gists = response.json()
+        # 生成任务 JSON
+        generate_task_json(gists)
+    else:
+        print(f"Error getting Gist ID. Aborting.")
