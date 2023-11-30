fec583f982a7abd29b4094438cf9346a60e4741a
czy13724 <103307567+czy13724@users.noreply.github.com>
2023-11-30 17:42:45 +0800

    Update gist_task_js-to-json.py

--- .github/scripts/gist/gist_task_js-to-json.py
+++ .github/scripts/gist/gist_task_js-to-json.py
@@ -63,8 +63,7 @@ def generate_task_json():
                 # 判断是否有配置文件，决定是否添加 addons 字段
                 if conf_file:
                     # 如果有配置文件，则添加 addons 字段
-                    js_filename = js_file["filename"].rsplit(".", 1)[0]
-                    task_entry["addons"] = f"https://gist.githubusercontent.com/{github_username}/{gist_id}/raw/main/{conf_file['filename']}, tag={js_filename}"
+                    task_entry["addons"] = f"https://gist.githubusercontent.com/{github_username}/{gist_id}/raw/main/{conf_file['filename']}, tag={file_name_without_extension}"
 
                 # 寻找对应的图片文件名
                 image_filename = f"{file_name_without_extension}.png"
