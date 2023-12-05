e5a6a5ef9a97667cf7e2be493b646e9bff4c2520
czy13724 <103307567+czy13724@users.noreply.github.com>
2023-12-05 18:19:44 +0800

    Update felotranslator.js

--- javascript/felotranslator.js
+++ javascript/felotranslator.js
@@ -1,13 +1,5 @@
 /*
 [rewrite_local]
-https://translator.felo.me/api/plan/current url script-response-body https://raw.githubusercontent.com/czy13724/TEST/main/javascript/felotranslator.js
-
-[mitm]
-hostname = translator.felo.me
-*/
-
-/*
-[rewrite_local]
 ^https:\/\/translator\.felo\.me\/api\/plan\/current$ url script-response-body https://raw.githubusercontent.com/czy13724/TEST/main/javascript/felotranslator.js
 
 [mitm]
@@ -36,8 +28,5 @@ var Q = JSON.parse(response.body);
     },
     "code": "OK"
   };
-    Q.data.lifetime_subscription = true;
-    Q.data.store_subscription = true;
-    Q.data.subscription = true;
-    $done({ body: JSON.stringify(Q) });
+$done({ body: JSON.stringify(Q) });
 
