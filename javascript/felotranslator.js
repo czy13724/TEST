9f632c5cc76196ea24713f2ffef3ec681cd00243
czy13724 <103307567+czy13724@users.noreply.github.com>
2023-12-05 12:29:23 +0800

    Update felotranslator.js

--- javascript/felotranslator.js
+++ javascript/felotranslator.js
@@ -5,36 +5,8 @@
 [mitm]
 hostname = translator.felo.me
 */
-
-const url = `https://translator.felo.me/api/plan/current`;
-const method = `GET`;
-const headers = {
-  'deviceId': `EBFFBD3C-125B-4CD4-99C1-C6E8255617AC`,
-  'Cookie': `fid=87e254b9-d843-4378-914e-f113569c7924`,
-  'Connection': `keep-alive`,
-  'Content-Type': `application/json`,
-  'Accept': `*/*`,
-  'Host': `translator.felo.me`,
-  'Accept-Encoding': `gzip, deflate, br`,
-  'Authorization': `x9_f9f7b1102d5f43189cdfb3e04eee2051__`,
-  'Accept-Language': `en-US,en;q=0.9`,
-  'User-Agent': `FeloTranslator/55 CFNetwork/1410.0.3 Darwin/22.6.0`
-};
-const body = ``;
-
-const myRequest = {
-  url: url,
-  method: method,
-  headers: headers,
-  body: body
-};
-
-$task.fetch(myRequest).then(response => {
-  console.log(response.statusCode + "\n\n" + response.body);
-
-
-  var Q = JSON.parse(response.body);
-  Q = {
+var Q = JSON.parse(response.body);
+Q = {
     "status": 200,
     "data": {
       "active_time": "2023-12-05T00:56:04+0000",
@@ -56,11 +28,4 @@ $task.fetch(myRequest).then(response => {
     },
     "code": "OK"
   };
-
-  console.log(response.statusCode + "\n\n" + JSON.stringify(Q));
-  $done();
-
-}, reason => {
-  console.log(reason.error);
-  $done();
-});
+$done({ body: JSON.stringify(Q) });
