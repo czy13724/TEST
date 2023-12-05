c112a3db0a6681081657d04a74a20427b5ead3ef
czy13724 <103307567+czy13724@users.noreply.github.com>
2023-12-05 20:51:53 +0800

    Update hax.js

--- javascript/hax.js
+++ javascript/hax.js
@@ -1,33 +1,18 @@
 /*
 [rewrite_local]
-^https:\/\/translator\.felo\.me\/api\/plan\/current$ url script-response-body https://raw.githubusercontent.com/czy13724/TEST/main/javascript/felotranslator.js
+^https:\/\/translator\.felo\.me\/api\/plan\/current$ url script-response-body https://raw.githubusercontent.com/czy13724/TEST/main/javascript/hax.js
 
 [mitm]
 hostname = translator.felo.me
 */
 
-var Q = JSON.parse($response.body);
-Q = {
-    "status": 200,
-    "data": {
-        "active_time": "2023-12-05T00:56:04+0000",
-        "expire_time": "2099-12-06T00:56:04+0000",
-        "now": "2023-12-05T01:08:33+0000",
-        "user_product_total": [
-            {
-                "freeze": 0,
-                "total_limit": "UNLIMITED",
-                "deduction_type": "DURATIONS",
-                "user_id": "1731839812985470978",
-                "product_item_type": "TRANSLATOR_BENEFIT",
-                "total": 0,
-                "balance": 0,
-                "user_product_total_id": "1731839819249618945"
-            }
-        ],
-        "pro": true
-    },
-    "code": "OK"
-};
+let obj = JSON.parse($response.body);
+    
+   
+    //obj.data.username = "我是叼毛安妮";  
 
-console.log(JSON.stringify(Q));
+    obj.data.pro = true;  
+
+    $done({
+        body: JSON.stringify(obj)
+    });
