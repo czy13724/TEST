d5066b74a8a8256adde651830a11b43208ed217f
czy13724 <103307567+czy13724@users.noreply.github.com>
2023-12-05 11:19:24 +0800

    Update felotranslator.js

--- javascript/felotranslator.js
+++ javascript/felotranslator.js
@@ -1,36 +1,12 @@
+/**********************************
+[rewrite_local]
+^https:\/\/translator\.felo\.me\/api\/plan\/current$ url script-response-body https://raw.githubusercontent.com/czy13724/TEST/main/javascript/felotranslator.js
 
-/**
- * @fileoverview Template to compose HTTP reqeuest.
- * 
- */
+[mitm]
+hostname = translator.felo.me
 
-const url = `https://translator.felo.me/api/plan/current`;
-const method = `GET`;
-const headers = {
-  'deviceId': `EBFFBD3C-125B-4CD4-99C1-C6E8255617AC`,
-  'Cookie': `fid=87e254b9-d843-4378-914e-f113569c7924`,
-  'Connection': `keep-alive`,
-  'Content-Type': `application/json`,
-  'Accept': `*/*`,
-  'Host': `translator.felo.me`,
-  'Accept-Encoding': `gzip, deflate, br`,
-  'Authorization': `x9_f9f7b1102d5f43189cdfb3e04eee2051__`,
-  'Accept-Language': `en-US,en;q=0.9`,
-  'User-Agent': `FeloTranslator/55 CFNetwork/1410.0.3 Darwin/22.6.0`
-};
-const body = ``;
 
-const myRequest = {
-  url: url,
-  method: method,
-  headers: headers,
-  body: body
-};
-
-$task.fetch(myRequest).then(response => {
-  console.log(response.statusCode + "\n\n" + response.body);
-
-  // Your second code snippet logic
+**********************************
   var Q = JSON.parse(response.body);
   Q = {
     "status": 200,
