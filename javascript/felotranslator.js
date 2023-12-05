a3f3f6f7607b2e11b0aa0094dc42522fdf7eca3e
czy13724 <103307567+czy13724@users.noreply.github.com>
2023-12-05 13:35:48 +0800

    Update felotranslator.js

--- javascript/felotranslator.js
+++ javascript/felotranslator.js
@@ -5,32 +5,33 @@
 [mitm]
 hostname = translator.felo.me
 */
-var Q = JSON.parse(response.body);
-Q = {
-    "status": 200,
-    "data": {
-        "active_time": "2023-12-05T00:56:04+0000",
-        "expire_time": "2099-12-06T00:56:04+0000",
-        "now": "2023-12-05T01:08:33+0000",
-        "user_product_total": [
-            {
-                "freeze": 0,
-                "total_limit": "UNLIMITED",
-                "deduction_type": "DURATIONS",
-                "user_id": "1731839812985470978",
-                "product_item_type": "TRANSLATOR_BENEFIT",
-                "total": 0,
-                "balance": 0,
-                "user_product_total_id": "1731839819249618945"
-            }
-        ],
-        "pro": true,
-        "user": {
-            "lifetime_subscription": true,
-            "store_subscription": true,
-            "subscription": true
-        }
-    },
-    "code": "OK"
+
+var responseJSON = {
+  "status": 200,
+  "data": {
+    "active_time": "2023-12-05T00:56:04+0000",
+    "expire_time": "2099-12-06T00:56:04+0000",
+    "now": "2023-12-05T01:08:40+0000",
+    "user_product_total": [
+      {
+        "freeze": 0,
+        "total_limit": "UNLIMITED",
+        "deduction_type": "DURATIONS",
+        "user_id": "1731839812985470978",
+        "product_item_type": "TRANSLATOR_BENEFIT",
+        "total": 0,
+        "balance": 0,
+        "user_product_total_id": "1731839819249618945"
+      }
+    ],
+    "pro": true,
+    "user": {
+      "lifetime_subscription": true,
+      "store_subscription": true,
+      "subscription": true
+    }
+  },
+  "code": "OK"
 };
-$done({ body: JSON.stringify(Q) });
+
+$done({ body: JSON.stringify(responseJSON) });
