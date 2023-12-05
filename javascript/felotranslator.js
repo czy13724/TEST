8fb1e71e339b63c1ed12aeb578f0e1c5360252ba
czy13724 <103307567+czy13724@users.noreply.github.com>
2023-12-05 18:15:56 +0800

    Update felotranslator.js

--- javascript/felotranslator.js
+++ javascript/felotranslator.js
@@ -14,6 +14,28 @@ hostname = translator.felo.me
 hostname = translator.felo.me
 */
 var Q = JSON.parse(response.body);
+  Q = {
+    "status": 200,
+    "data": {
+      "active_time": "2023-12-05T00:56:04+0000",
+      "expire_time": "2099-12-06T00:56:04+0000",
+      "now": "2023-12-05T01:08:33+0000",
+      "user_product_total": [
+        {
+          "freeze": 0,
+          "total_limit": "UNLIMITED",
+          "deduction_type": "DURATIONS",
+          "user_id": "1731839812985470978",
+          "product_item_type": "TRANSLATOR_BENEFIT",
+          "total": 0,
+          "balance": 0,
+          "user_product_total_id": "1731839819249618945"
+        }
+      ],
+      "pro": true
+    },
+    "code": "OK"
+  };
     Q.data.lifetime_subscription = true;
     Q.data.store_subscription = true;
     Q.data.subscription = true;
