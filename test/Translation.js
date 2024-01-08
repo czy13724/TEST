3ce955b6bc5e89444159d86f59aeb9ac784fdffb
Levi <103307567+czy13724@users.noreply.github.com>
2024-01-08 12:14:11 -0500

    Create Translation.js

---
+++ test/Translation.js
@@ -0,0 +1,17 @@
+/*
+translator
+功能未测试
+2024.01.08
+
+[rewrite_local]
+http://www\.xslm333\.xyz/index\.php\?s=/api/xuser/getUserInfo url script-response-body translator.js
+
+[Mitm]
+hostname = www.xslm333.xyz
+*/
+
+let Levi = JSON.parse($response.body);
+Levi.data.status = 1;
+Levi.data.is_long = 1;
+Levi.data.filings_nums = 1;
+$done({body: JSON.stringify(Levi)});
