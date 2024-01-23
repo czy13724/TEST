b364cf7610cfb0c4759925ed64308e7f7ed68101
Levi <103307567+czy13724@users.noreply.github.com>
2024-01-23 09:38:56 -0500

    only for tests

---
+++ qxjs/felotranslator.js
@@ -0,0 +1,18 @@
+/*
+[rewrite_local]
+https://translator.felo.me/api/plan/current url script-response-body https://raw.githubusercontent.com/czy13724/TEST/main/javascript/felotranslator.js
+
+[mitm]
+hostname = translator.felo.me
+*/
+
+let obj = JSON.parse($response.body);
+    
+   
+    obj.data.expire_time = "2099-12-06T00:56:04+0000";  
+
+    obj.data.pro = true;  
+
+    $done({
+        body: JSON.stringify(obj)
+    });
