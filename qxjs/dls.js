cf0e7f325e452bd9bbed8cb89807665d7d745d26
Levi <103307567+czy13724@users.noreply.github.com>
2024-02-18 19:13:17 -0500

    Update dls.js

--- qxjs/dls.js
+++ qxjs/dls.js
@@ -127,7 +127,7 @@ async signin() {
                     "content-type": "application/json;charset=utf-8",
                     "User-Agent": "Mozilla/5.0 (iPad; CPU OS 16_6_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 MicroMessenger/8.0.47(0x18002f28) NetType/WIFI Language/zh_CN",
                     "Access-Token":this.token,
-                    "sid":10006,
+                    "sid": "10006",
                 },
                 body: {}
             };
@@ -163,7 +163,7 @@ async point() {
                     "content-type": ";application/json;charset=utf-8",
                     "User-Agent": "Mozilla/5.0 (iPad; CPU OS 16_6_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 MicroMessenger/8.0.47(0x18002f28) NetType/WIFI Language/zh_CN",
                     "Access-Token":this.token,
-                    "sid":10006,
+                    "sid": "10006",
                 },
                 //body: {}
             };
