a927a9189454bb4f85c40d29f3899cb75b2c99ce
Levi <103307567+czy13724@users.noreply.github.com>
2025-06-17 23:45:07 +0800

    Update xbackvpn.js

--- qxjs/xbackvpn.js
+++ qxjs/xbackvpn.js
@@ -1,7 +1,7 @@
 #!name=FoodiePro解锁
 #!desc=FoodiePro解锁
 [Script]
-FoodiePro解锁 = type=http-response,pattern=^https:\/\/purchase-foodie-api\.snow\.me\/v1\/purchase\/subscription\/subscriber\/status,requires-body=1,script-path=https://raw.githubusercontent.com/czy13724/TEST/main/qxjs/test.js,script-update-interval=0,engine=webview
+FoodiePro解锁 = type=http-response, pattern=^https:\/\/purchase-foodie-api\.snow\.me\/v1\/purchase\/subscription\/subscriber\/status, requires-body=true, script-path=https://raw.githubusercontent.com/czy13724/TEST/main/qxjs/test.js
 
 [mitm]
 hostname = purchase-foodie-api.snow.me
