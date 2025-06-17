dbabc6684e32fed7548ffe91d3269b04328c2470
Levi <103307567+czy13724@users.noreply.github.com>
2025-06-17 23:53:52 +0800

    Delete qxjs/xbackvpn.js

--- qxjs/xbackvpn.js
+++
@@ -1,7 +0,0 @@
-#!name=FoodiePro解锁
-#!desc=FoodiePro解锁
-[Script]
-FoodiePro解锁 = type=http-response, pattern=^https:\/\/purchase-foodie-api\.snow\.me\/v1\/purchase\/subscription\/subscriber\/status, requires-body=true, script-path=https://raw.githubusercontent.com/czy13724/TEST/main/qxjs/test.js
-
-[mitm]
-hostname = purchase-foodie-api.snow.me
